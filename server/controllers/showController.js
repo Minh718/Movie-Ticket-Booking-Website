436cfddc7d7ca0f546b583fed961d1daf86e6d4c
NgoTTrong <trongngo08082008@gmail.com>
2022-12-13 21:48:55 +0700

    Connect quick booking to backend

--- server/controllers/showController.js
+++ server/controllers/showController.js
@@ -1,11 +1,21 @@
 const createConnection = require('../config/database')
 
 const showController = {
+    getAllShows : async (req,res) => {
+        const connection = await createConnection();
+        try{
+            const sql = `SELECT idShow,idMovie,price,room,title FROM tbl_show S
+            JOIN tbl_movie M
+            ON S.idMovie = M.id
+            `
+            console.log(sql);
+            const result = await connection.query(sql);
+            res.status(200).json(result);
+        }catch(err){
+            res.status(500).json(err);
+        }
+    },
     addShow : async (req,res) =>{
-        const idMovie = req.body.idMovie;
-        const price = req.body.price;
-        const room = req.body.room;
-
         const connection = await createConnection();
         try{
             const sql = `INSERT INTO tbl_show (idMovie, price, room) VALUES (${idMovie},${price},'${room}')`
