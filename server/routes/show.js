29deb38587b780981464fad4edac096ce8ee1ebe
NgoTTrong <trongngo08082008@gmail.com>
2022-12-15 14:46:20 +0700

    add show management

--- server/routes/show.js
+++ server/routes/show.js
@@ -3,5 +3,6 @@ const showController = require('../controllers/showController');
 
 showRouter.post("/add",showController.addShow)
 showRouter.get("/",showController.getAllShows)
+showRouter.post("/delete",showController.deleteShow)
 
 module.exports = showRouter;
